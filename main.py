6a4312cb0cbb357eac745fc41be38db3de777513
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-01-14 01:03:50 +0300

    exam search
    cringe but works))

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from config import TELEGRAM_TOKEN
 import logging
 from datetime import datetime
 
-group_pattern = r'[А-Яа-яA-Za-z]{4}-\d{2}-\d{2}'
+group_pattern = r'[А-Яа-я]{4}-\d{2}-\d{2}'
+exam_pattern = r'экз (.+)|Экз (.+)|ЭКЗ (.+)'
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
@@ -12,7 +13,16 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Привет!\nНа период сессии включен режим экзаменов.\nПоддерживается поиск как по "
                                   "фамилии,"
-                                  "так и по группе.\n\nПримеры:\n\nКарпов\nКарпов Д.А\nКТСО-04-20\n")
+                                  "так и по группе.\n\nПримеры:\n\nКарпов\nКарпов Д.А\nКТСО-04-20\n\nА для поиска "
+                                  "экзаменов по дисциплине введите\nэкз <название дисциплины> без кавычек")
+
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Поддерживается поиск как по "
+                                  "фамилии,"
+                                  "так и по группе.\n\nПримеры:\n\nКарпов\nКарпов Д.А\nКТСО-04-20\n\nА для поиска "
+                                  "экзаменов по дисциплине введите\nэкз <название дисциплины> без кавычек")
 
 
 def search(update, context):
@@ -115,12 +125,61 @@ def group_search(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text=text[i: i + 4096], parse_mode='Markdown')
 
 
+def exam_search(update, context):
+    with open('exams.json', 'r') as f:
+        exams = json.load(f)
+    examname = update.message.text
+    examname = examname[5:]
+    exam_ids = [exam_id for exam_id, exam in exams['exam'].items() if examname.lower() in exam.lower()]
+
+    unique_exams = {}
+
+    for exam_id in exam_ids:
+        if (exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id]) in unique_exams:
+            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])][
+                'groups'].append(exams['group'][exam_id])
+        else:
+            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])] = {
+                'exam': exams["exam"][exam_id],
+                'room': exams["rooms"][exam_id],
+                'time': exams["time_start"][exam_id],
+                'day': exams["day"][exam_id],
+                'month': exams["month"][exam_id],
+                'groups': [exams['group'][exam_id]],
+                'extype': exams["exam_type"][exam_id],
+                'teacher': exams["teachers"][exam_id]
+            }
+    sorted_exams = sorted(unique_exams.values(), key=lambda x: (x['month'], x['day'], x['time']))
+    text = ''
+    for exam in sorted_exams:
+        groups_str = ', '.join(exam['groups'])
+        date_str = '2023-01-[day]'.replace('[day]', str(exam['day']))
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+        weekday = date.weekday()
+        weekday_str = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'][weekday]
+        text += f'Дата: {exam["day"]} Января ({weekday_str})\n'
+        text += f'🧑‍🏫 Преподаватель: {exam["teacher"]}\n'
+        text += f'🕜 Время: {exam["time"]}\n'
+        text += f'📚 {exam["extype"]}\n'
+        text += f'📝 {exam["exam"]}\n'
+        text += f'🏫 Аудитория: {exam["room"]}\n'
+        text += f'👥 Группы: {groups_str}\n\n'
+    if not exam_ids:
+        text = 'Экзамен не найден'
+    text_len = len(text)
+    for i in range(0, text_len, 4096):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text[i: i + 4096], parse_mode='Markdown')
+
+
 def main():
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text & Filters.regex(group_pattern), group_search))
-    dp.add_handler(MessageHandler(Filters.text, search))
+
+    dp.add_handler(CommandHandler('start', start, run_async=True))
+    dp.add_handler(CommandHandler('help', help, run_async=True))
+    dp.add_handler(MessageHandler(Filters.regex(group_pattern), group_search, run_async=True))
+    dp.add_handler(MessageHandler(Filters.regex(exam_pattern), exam_search, run_async=True))
+    dp.add_handler(MessageHandler(Filters.text, search, run_async=True))
 
     updater.start_polling()
 
