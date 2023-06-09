43d75f4ee18797677e5ad81008dac987b035c6ac
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-06-09 23:59:22 +0300

    update: refactored

--- bot/main.py
+++ bot/main.py
@@ -1,280 +1,194 @@
 import json
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from config import TELEGRAM_TOKEN
-from config import cmstoken
 import logging
-from datetime import datetime
+import re
+import datetime
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from decode import decode_teachers
+from config import TELEGRAM_TOKEN
+from lazy_logger import lazy_logger
 
-group_pattern = r'[А-Яа-я]{4}-\d{2}-\d{2}'
-exam_pattern = r'экз (.+)|Экз (.+)|ЭКЗ (.+)'
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
-extypes = {
-    'экзамен': 'Экзамен',
-    'консультация': 'Консультация',
+MONTHS = {
+    1: "Января",
+    2: "Февраля",
+    3: "Марта",
+    4: "Апреля",
+    5: "Мая",
+    6: "Июня",
+    7: "Июля",
+    8: "Августа",
+    9: "Сентября",
+    10: "Октября",
+    11: "Ноября",
+    12: "Декабря"
 }
+
+GROUP_PATTERN = r'[А-Яа-я]{4}-\d{2}-\d{2}'
+EXAM_PATTERN = r'экз (.+)|Экз (.+)|ЭКЗ (.+)'
 
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Привет!\nНа период сессии включен режим экзаменов.\nПоддерживается поиск как по "
-                                  "фамилии,"
-                                  "так и по группе.\n\nПримеры:\n\nКарпов\nКарпов Д.А\nКТСО-04-20\n\nА для поиска "
-                                  "экзаменов по дисциплине введите\nэкз <название дисциплины> без кавычек")
-
-
-def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Поддерживается поиск как по "
-                                  "фамилии,"
-                                  "так и по группе.\n\nПримеры:\n\nКарпов\nКарпов Д.А\nКТСО-04-20\n\nА для поиска "
-                                  "экзаменов по дисциплине введите\nэкз <название дисциплины> без кавычек")
+                             text="Привет!\nНа период сессии включен режим сессии.\n\n"
+                                  "Доступен поиск по преподавателям и группам.\n"
+                                  "Введи фамилию преподавателя или группу.\n\n"
+                                  "Примеры: \n"
+                                  "`Иванов И.А.`\n"
+                                  "`Иванов`\n"
+                                  "`ИВБО-07-22`", parse_mode='Markdown')
 
 
 def search(update, context):
-    with open('data/exams.json', 'r', encoding='utf-8') as f:
-        exams = json.load(f)
+    query = update.message.text
+    mode = determine_search_mode(query)
 
-    last_name = update.message.text
-    if len(last_name) < 3:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Слишком короткая фамилия")
+    if mode == 'teacher':
+        query = prepare_teacher_query(query)
+    elif mode == 'group':
+        query = query.lower()
+
+    if len(query) < 3:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Слишком короткий запрос")
         return
-    exam_ids = [exam_id for exam_id, teacher in exams['teachers'].items() if last_name.lower() in teacher.lower()]
 
-    unique_exams = {}
+    lazy_logger.info(json.dumps({"type": "request", "query": query.lower(), **update.message.from_user.to_dict()},
+                                ensure_ascii=False))
 
-    for exam_id in exam_ids:
-        if (exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id]) in unique_exams:
-            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])][
-                'groups'].append(exams['group'][exam_id])
-        else:
-            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])] = {
-                'exam': exams["exam"][exam_id],
-                'room': exams["rooms"][exam_id],
-                'time': exams["time_start"][exam_id],
-                'day': exams["day"][exam_id],
-                'month': exams["month"][exam_id],
-                'groups': [exams['group'][exam_id]],
-                'extype': exams["exam_type"][exam_id],
-                'teacher': exams["teachers"][exam_id]
-            }
+    exams = load_exams_from_file()
 
-    sorted_exams = sorted(unique_exams.values(), key=lambda x: (x['month'], x['day'], x['time']))
-
-    text = ''
-    for exam in sorted_exams:
-        groups_str = ', '.join(exam['groups'])
-        date_str = '2023-01-[day]'.replace('[day]', str(exam['day']))
-        date = datetime.strptime(date_str, '%Y-%m-%d')
-        weekday = date.weekday()
-        weekday_str = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'][weekday]
-        import requests
-        rawNames = [exam['teacher']]
-        headers = {
-            "Authorization": f"Bearer {cmstoken}"}
-        params = {"rawNames": rawNames}
-
-        response = requests.get("https://cms.mirea.ninja/api/get-full-teacher-name", headers=headers, params=params)
-        if response.status_code == 200:
-            data = response.json()
-
-            decoded_names = []
-            for names in data:
-                if len(names["possibleFullNames"]) == 1:
-                    decomposed_name = names["possibleFullNames"][0]
-                    name = []
-                    if surname := decomposed_name.get("lastName"):
-                        name.append(surname)
-                    if first_name := decomposed_name.get("firstName"):
-                        name.append(first_name)
-                    if middle_name := decomposed_name.get("middleName"):
-                        name.append(middle_name)
-                    name = " ".join(name)
-                else:
-                    name = names["rawName"]
-                decoded_names.append(name)
-
-            decoded_names = ", ".join(decoded_names)
-        else:
-            decoded_names = ", ".join(rawNames)
-        text += f'Дата: {exam["day"]} Января ({weekday_str})\n'
-        text += f'🧑‍🏫 Преподаватель: {decoded_names}\n'
-        text += f'🕜 Время: {exam["time"]}\n'
-        text += f'📚 {extypes[exam["extype"]]}\n'
-        text += f'📝 {exam["exam"]}\n'
-        text += f'🏫 Аудитория: {exam["room"]}\n'
-        text += f'👥 Группы: {groups_str}\n\n'
+    exam_ids = find_exam_ids(query, exams, mode)
 
     if not exam_ids:
-        text = 'Преподаватель не найден'
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="По вашему запросу ничего не найдено")
+        return
 
-    text_len = len(text)
-    for i in range(0, text_len, 4096):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text[i: i + 4096], parse_mode='Markdown')
+    unique_exams = create_unique_exams(exam_ids, exams)
+    sorted_exams = sort_exams(unique_exams)
+
+    if mode == 'teacher':
+        context.user_data['teacher'] = query
+        surnames_count = check_same_surnames(sorted_exams, update, context)
+        if surnames_count:
+            return
+
+    send_exam_info(update, context, sorted_exams, mode)
 
 
-def group_search(update, context):
+def check_same_surnames(sorted_exams, update, context):
+    surnames = list(set([exam[1]['teacher'] for exam in sorted_exams]))
+    surnames_str = ', '.join(decode_teachers(surnames))
+    surnames_count = len(surnames)
+    if surnames_count > 1:
+        keyboard = [[surname] for surname in surnames]
+        reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f"По запросу ({context.user_data['teacher'][:-1].title()}) найдено несколько "
+                                      f"преподавателей:\n\n({surnames_str})"
+                                      f"\n\nУточните запрос:",
+                                 reply_markup=reply_markup)
+        return surnames_count
+
+
+def determine_search_mode(query):
+    if re.match(GROUP_PATTERN, query):
+        return 'group'
+    return 'teacher'
+
+
+def prepare_teacher_query(query):
+    if " " not in query:
+        query += " "
+    return query.lower()
+
+
+def load_exams_from_file():
     with open('data/exams.json', 'r', encoding='utf-8') as f:
         exams = json.load(f)
+    return exams
 
-    group_name = update.message.text
-    exam_ids = [exam_id for exam_id, group in exams['group'].items() if group_name.lower() in group.lower()]
 
+def find_exam_ids(query, exams, mode):
+    if mode == 'teacher':
+        exam_ids = [exam_id for exam_id, teacher in exams['teachers'].items() if query in teacher.lower()]
+    else:
+        exam_ids = [exam_id for exam_id, group in exams['group'].items() if query == group.lower()]
+    return exam_ids
+
+
+def create_unique_exams(exam_ids, exams):
     unique_exams = {}
-
     for exam_id in exam_ids:
-        if (exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id]) in unique_exams:
-            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])][
-                'groups'].append(exams['group'][exam_id])
+        key = (exams['day'][exam_id], exams['time_start'][exam_id], exams['month'][exam_id])
+        if key in unique_exams:
+            unique_exams[key]['group'].append(exams['group'][exam_id])
         else:
-            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])] = {
-                'exam': exams["exam"][exam_id],
-                'room': exams["rooms"][exam_id],
-                'time': exams["time_start"][exam_id],
-                'day': exams["day"][exam_id],
-                'month': exams["month"][exam_id],
-                'groups': [exams['group'][exam_id]],
-                'extype': exams["exam_type"][exam_id],
-                'teacher': exams["teachers"][exam_id]
+            unique_exams[key] = {
+                'exam': exams['exam'][exam_id],
+                'group': [exams['group'][exam_id]],
+                'teacher': exams['teachers'][exam_id],
+                'room': exams['rooms'][exam_id] if exams['rooms'][exam_id] else "",
+                'day': exams['day'][exam_id],
+                'month': exams['month'][exam_id],
+                'time_start': exams['time_start'][exam_id],
+                'type': exams['exam_type'][exam_id] if exams['exam_type'][exam_id] else ""
             }
+    print(unique_exams)
+    return unique_exams
 
-    sorted_exams = sorted(unique_exams.values(), key=lambda x: (x['month'], x['day'], x['time']))
 
-    text = ''
+def sort_exams(unique_exams):
+    return sorted(unique_exams.items(), key=lambda x: (x[1]['month'], x[1]['day'], x[1]['time_start']))
+
+
+def send_exam_info(update, context, sorted_exams, mode):
+    chunks = []
+    chunk = ""
+
     for exam in sorted_exams:
-        groups_str = ', '.join(exam['groups'])
-        date_str = '2023-01-[day]'.replace('[day]', str(exam['day']))
-        date = datetime.strptime(date_str, '%Y-%m-%d')
-        weekday = date.weekday()
-        weekday_str = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'][weekday]
-        other_groups = ""
-        for group_id, group in exams['group'].items():
-            if exams['day'][group_id] == exam['day'] and exams['month'][group_id] == exam['month'] and exams['rooms'][
-                group_id] == exam['room'] and exams['exam'][group_id] == exam['exam'] and exams['time_start'][
-                group_id] == exam['time'] and group not in exam['groups']:
-                other_groups += group + ", "
-        other_groups = other_groups[:-2]
-        import requests
-        rawNames = [exam['teacher']]
-        headers = {
-            "Authorization": f"Bearer {cmstoken}"}
-        params = {"rawNames": rawNames}
-
-        response = requests.get("https://cms.mirea.ninja/api/get-full-teacher-name", headers=headers, params=params)
-        if response.status_code == 200:
-            data = response.json()
-
-            decoded_names = []
-            for names in data:
-                if len(names["possibleFullNames"]) == 1:
-                    decomposed_name = names["possibleFullNames"][0]
-                    name = []
-                    if surname := decomposed_name.get("lastName"):
-                        name.append(surname)
-                    if first_name := decomposed_name.get("firstName"):
-                        name.append(first_name)
-                    if middle_name := decomposed_name.get("middleName"):
-                        name.append(middle_name)
-                    name = " ".join(name)
-                else:
-                    name = names["rawName"]
-                decoded_names.append(name)
-
-            decoded_names = ", ".join(decoded_names)
+        exam_info = format_exam_info(exam, mode)
+        if len(chunk) + len(exam_info) <= 4096:
+            chunk += exam_info
         else:
-            decoded_names = ", ".join(rawNames)
+            chunks.append(chunk)
+            chunk = exam_info
 
-        text += f'Дата: {exam["day"]} Января ({weekday_str})\n'
-        text += f'🧑‍🏫 Преподаватель: {decoded_names}\n'
-        text += f'🕜 Время: {exam["time"]}\n'
-        text += f'📚 {extypes[exam["extype"]]}\n'
-        text += f'📝 {exam["exam"]}\n'
-        text += f'🏫 Аудитория: {exam["room"]}\n'
-        text += f'👥 Группы: {groups_str}'
-        if other_groups:
-            text += f' (вместе с: {other_groups})'
-        text += '\n\n'
-    if not exam_ids:
-        text = 'Группа не найдена'
+    if chunk:
+        chunks.append(chunk)
 
-    text_len = len(text)
-    for i in range(0, text_len, 4096):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text[i: i + 4096], parse_mode='Markdown')
+    for chunk in chunks:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=chunk, reply_markup=ReplyKeyboardRemove())
 
 
-def exam_search(update, context):
-    with open('data/exams.json', 'r', encoding='utf-8') as f:
-        exams = json.load(f)
-    examname = update.message.text
-    examname = examname[4:]
-    exam_ids = [exam_id for exam_id, exam in exams['exam'].items() if examname.lower() in exam.lower()]
+def format_exam_info(exam, mode):
+    exam_info = ""
+    groups = ', '.join(exam[1]['group'])
+    date = exam[1]['day']
+    time_start = exam[1]['time_start']
+    room = exam[1]['room']
+    teacher = exam[1]['teacher']
+    teachers = ", ".join(decode_teachers([teacher]))
+    lesson = exam[1]['exam']
+    time_start = datetime.datetime.strptime(time_start, "%H:%M:%S").strftime("%H:%M")
+    month_name = MONTHS[int(exam[1]['month'])]
 
-    unique_exams = {}
+    formatted_time = f"{time_start}"
+    exam_info += f"📆 Дата: {date} {month_name}\n"
+    exam_info += f'⏰ Время: {formatted_time}\n'
+    exam_info += f"🏫 Аудитории: {room}\n"
+    exam_info += f'📝 {lesson}\n'
+    if len(groups) > 0:
+        exam_info += f'👥 Группы: {groups}\n'
+    if exam[1]['type']:
+        exam_info += f'📚 Тип: {exam[1]["type"]}\n'
+    if len(teachers) > 0:
+        exam_info += f"👨🏻‍🏫 Преподаватели: {teachers}\n\n"
+    else:
+        exam_info += "\n"
 
-    for exam_id in exam_ids:
-        if (exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id]) in unique_exams:
-            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])][
-                'groups'].append(exams['group'][exam_id])
-        else:
-            unique_exams[(exams['day'][exam_id], exams['month'][exam_id], exams['time_start'][exam_id])] = {
-                'exam': exams["exam"][exam_id],
-                'room': exams["rooms"][exam_id],
-                'time': exams["time_start"][exam_id],
-                'day': exams["day"][exam_id],
-                'month': exams["month"][exam_id],
-                'groups': [exams['group'][exam_id]],
-                'extype': exams["exam_type"][exam_id],
-                'teacher': exams["teachers"][exam_id]
-            }
-    sorted_exams = sorted(unique_exams.values(), key=lambda x: (x['month'], x['day'], x['time']))
-    text = ''
-    for exam in sorted_exams:
-        groups_str = ', '.join(exam['groups'])
-        date_str = '2023-01-[day]'.replace('[day]', str(exam['day']))
-        date = datetime.strptime(date_str, '%Y-%m-%d')
-        weekday = date.weekday()
-        weekday_str = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'][weekday]
-        import requests
-        rawNames = [exam['teacher']]
-        headers = {
-            "Authorization": f"Bearer {cmstoken}"}
-        params = {"rawNames": rawNames}
-
-        response = requests.get("https://cms.mirea.ninja/api/get-full-teacher-name", headers=headers, params=params)
-        if response.status_code == 200:
-            data = response.json()
-
-            decoded_names = []
-            for names in data:
-                if len(names["possibleFullNames"]) == 1:
-                    decomposed_name = names["possibleFullNames"][0]
-                    name = []
-                    if surname := decomposed_name.get("lastName"):
-                        name.append(surname)
-                    if first_name := decomposed_name.get("firstName"):
-                        name.append(first_name)
-                    if middle_name := decomposed_name.get("middleName"):
-                        name.append(middle_name)
-                    name = " ".join(name)
-                else:
-                    name = names["rawName"]
-                decoded_names.append(name)
-
-            decoded_names = ", ".join(decoded_names)
-        else:
-            decoded_names = ", ".join(rawNames)
-        text += f'Дата: {exam["day"]} Января ({weekday_str})\n'
-        text += f'🧑‍🏫 Преподаватель: {decoded_names}\n'
-        text += f'🕜 Время: {exam["time"]}\n'
-        text += f'📚 {extypes[exam["extype"]]}\n'
-        text += f'📝 {exam["exam"]}\n'
-        text += f'🏫 Аудитория: {exam["room"]}\n'
-        text += f'👥 Группы: {groups_str}\n\n'
-    if not exam_ids:
-        text = 'Экзамен не найден'
-    text_len = len(text)
-    for i in range(0, text_len, 4096):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text[i: i + 4096], parse_mode='Markdown')
+    return exam_info
 
 
 def main():
@@ -282,13 +196,11 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start, run_async=True))
-    dp.add_handler(CommandHandler('help', help, run_async=True))
-    dp.add_handler(MessageHandler(Filters.regex(group_pattern), group_search, run_async=True))
-    dp.add_handler(MessageHandler(Filters.regex(exam_pattern), exam_search, run_async=True))
+    dp.add_handler(CommandHandler('help', start, run_async=True))
+    dp.add_handler(CommandHandler('about', start, run_async=True))
     dp.add_handler(MessageHandler(Filters.text, search, run_async=True))
 
     updater.start_polling()
-
     updater.idle()
 
 
